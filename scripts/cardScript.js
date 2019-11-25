f33f8ca57d099779bd9cd1aad90d4b142f75d6e8
Dayan Trabanco Perez <dayan.trabancoperez@amista.be>
2019-11-25 13:56:01 +0100

    Add navigation to step on click div

--- scripts/cardScript.js
+++ scripts/cardScript.js
@@ -6,21 +6,25 @@ container.setAttribute('class', 'container')
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'http://localhost:8080/steps/', true)
 request.onload = function() {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(step => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
+      $('.card').click(function () {
+          window.location = 'step.html';
+      });
+
       const h1 = document.createElement('h1')
-      h1.textContent = movie.title
+      h1.textContent = step.name
 
       const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
+      step.description = step.description.substring(0, 300)
+      p.textContent = `${step.description}`
 
       container.appendChild(card)
       card.appendChild(h1)
