e0cfca401888e855505535c76dd0d924bab2fc0f
DayanTrabancoPerez <dayan.trabanco.perez@student.ehb.be>
2020-02-26 13:47:41 +0100

    add get in login

--- scripts/loginLogic.js
+++ scripts/loginLogic.js
@@ -1,12 +1,20 @@
 function submit() {
-  console.log("test");
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+//  var username = "dayanT";
+  //var password = "test125";
+
+
+  console.log(username);
   var request = new XMLHttpRequest()
-  request.open('GET', 'http://localhost:8080/users', true)
+  request.open('GET', 'http://localhost:8080/users/login/' + username + "," + password, true)
   request.onload = function() {
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
+      console.log(data);
         //backend aanpassen zodat 2 params kunnen ingevoerd worden en dus daarop checken voor de login
       //document.getElementById("textField").value = stepString;
+      alert("you are in");
     } else {
       const errorMessage = document.createElement('marquee')
       errorMessage.textContent = `Gah, it's not working!`
