2ba87beee6ce5d4b04413952d1b7c49b5a3df5da
DayanTrabancoPerez <dayan.trabanco.perez@student.ehb.be>
2020-03-05 13:33:52 +0100

    Fixing span delete pronblem

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -1,63 +1,37 @@
-$(document).ready(function() {
-
-});
-
+//globals that will be used onKeyClicked
 var correct = 0;
 var errors = 0;
 var currentLength = 20;
 
 function keyClicked(e) {
-  //alert("You pressed a key inside the input field");
-  // it will give the text of the span
-
   console.log(stepString.length);
-  var stirng = stepString.length;
-  //var text = document.getElementById("textField").value;
   var currentSpan = stepString.length - currentLength;
-
   console.log("this" + stepString.length + "-" + currentLength);
-
-  //var textLength = document.getElementById("textField").value.length;
-  //var aantalkerenGeprobeerd;
-  //if(text[0] === )
   var keynum;
   if (window.event) { // IE
     keynum = e.keyCode;
-    /*if(keynum === 32) {
-      keynum = " ";
-    } */
   } else if (e.which) { // Netscape/Firefox/Opera
     keynum = e.which;
   }
   console.log("the pressed key" + "'" + String.fromCharCode(keynum).trim() + "'");
   console.log("the correct key needed" + "'" + document.getElementById("letter_" + currentSpan).innerText + "'");
-  console.log("curren" + currentSpan);
-  console.log("currentLength" + currentLength);
+  console.log("current letter : " + currentSpan);
+  console.log("currentLength of the string :" + currentLength);
   if (document.getElementById("letter_" + currentSpan).innerText === String.fromCharCode(keynum).trim()) {
-    //document.getElementById("textField").value.replace(/^/,'');
-    //text = text.replace(text[0], '')
+    //if correct we will reduce the length of the string
     currentLength--;
     correct++;
+    //will remove the span when correct
     var span = document.getElementById("letter_" + currentSpan);
-    $('span[id^="letter_' + currentSpan + '"]').remove();
-
-    //span.parentNode.removeChild(span)
-    //document.getElementById("textField").value = text;
-    //document.getElementById("textField").style.color = "Black";
+    span.remove();
   } else {
     errors++;
     document.getElementById("letter_" + currentSpan).style.background = "pink";
     document.getElementById("letter_" + currentSpan).style.color = "darkred";
     document.getElementById("letter_" + currentSpan).style.border.bottom = "1px solid Red";
-    //document.getElementById("letter_" + currentSpan).classList.add('error');
-
-    /* setTimeout(function() {
-        document.getElementById("textField").classList.remove('error');
-    }, 300); */
   }
   if (currentLength < 1) {
     alert("Errors:" + errors + " & correct first try: " + correct + "on a string of :" + stepString.length);
     window.location = 'home.html';
   }
-  //
 }
