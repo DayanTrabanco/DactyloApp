848dec727af094bfcc5a0461fc31b3c4897f96e6
DayanTrabancoPerez <dayan.trabanco.perez@student.ehb.be>
2020-03-12 23:17:45 +0100

    added features to leaderboard page

--- scripts/leaderboards.js
+++ scripts/leaderboards.js
@@ -8,26 +8,28 @@ tbl.setAttribute('border', '1');
 container.appendChild(tbl);
 app.appendChild(container);
 
+const dropdown = document.getElementById("dropdown");
 
-var request = new XMLHttpRequest()
-request.open('GET', 'http://localhost:8080/scores/top/step 1', true)
-request.onload = function() {
+
+
+var request1 = new XMLHttpRequest()
+request1.open('GET', 'http://localhost:8080/steps', true)
+request1.onload = function() {
   // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(step => {
-      var tr = document.createElement('tr');
-      var td = document.createElement('td');
-      td.innerText = step.score;
+  var data1 = JSON.parse(this.response)
+  if (request1.status >= 200 && request1.status < 400) {
+    data1.forEach(step => {
+      console.log(step);
+      const anchor = document.createElement('a');
+      anchor.innerText = step.name;
+      anchor.setAttribute("class", "dropdown-item");
 
-
-      var user = document.createElement('td');
-      user.innerText = step.username;
-
-      tr.appendChild(user);
-      tr.appendChild(td);
-      tbl.appendChild(tr);
+      dropdown.appendChild(anchor);
     })
+
+    $('.dropdown-item').click(function (e) {
+        showLeaderboard();
+    });
 
   } else {
     const errorMessage = document.createElement('marquee')
@@ -35,5 +37,46 @@ request.onload = function() {
     app.appendChild(errorMessage)
   }
 }
+request1.send();
 
-request.send()
+function showLeaderboard() {
+  //leaderboard step 1
+  var request = new XMLHttpRequest()
+  request.open('GET', 'http://localhost:8080/scores/top/step 1', true)
+  request.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+    if (request.status >= 200 && request.status < 400) {
+      data.forEach(score => {
+        var tr = document.createElement('tr');
+        var highScore = document.createElement('td');
+        var user = document.createElement('td');
+        var correct = document.createElement('td');
+        var errors = document.createElement('td');
+        var time = document.createElement('td');
+        var date = document.createElement('td');
+
+        highScore.innerText = score.score;
+        correct.innerText = score.totalCorrect;
+        user.innerText = score.username;
+        errors.innerText = score.totalErrors;
+        time.innerText = score.timeNeeded;
+        date.innerText = score.createdOn;
+
+        tr.appendChild(user);
+        tr.appendChild(correct);
+        tr.appendChild(errors);
+        tr.appendChild(time);
+        tr.appendChild(highScore);
+        tr.appendChild(date);
+        tbl.appendChild(tr);
+      })
+
+    } else {
+      const errorMessage = document.createElement('marquee')
+      errorMessage.textContent = `Gah, it's not working!`
+      app.appendChild(errorMessage)
+    }
+  }
+  request.send()
+}
--- scripts/logout.js
+++ scripts/logout.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
   if(window.localStorage.getItem("user") !== "") {
     document.getElementById("loginBtn").style.display = 'none';
     document.getElementById("logoutBtn").style.display = 'block';
+
+  //  document.getElementById("currentUser").style.display = 'block';
+  //  document.getElementById("currentUser").innerText = window.localStorage.getItem("user");
   } else {
-    document.getElementById("loginBtn").style.display = 'block';
-    document.getElementById("logoutBtn").style.display = 'none';
+  //  document.getElementById("loginBtn").style.display = 'block';
+  //  document.getElementById("logoutBtn").style.display = 'none';
+  //  document.getElementById("currentUser").style.display = 'none';
   }
   document.getElementById("logoutBtn").addEventListener("click", function() {
     logout();
