202ab74bcd273198a2e41e49cdc40ff975322f09
DayanTrabancoPerez <dayan.trabanco.perez@student.ehb.be>
2020-04-28 20:41:30 +0200

    Leaderboard changed to have multiple options

--- scripts/leaderboards.js
+++ scripts/leaderboards.js
@@ -1,5 +1,13 @@
 const app = document.getElementById('root')
 
+const userOption = document.getElementById("me");
+if(localStorage.getItem("user") !== "") {
+  userOption.innerText = localStorage.getItem("user");
+  userOption.style.display = "block";
+} else {
+  userOption.style.display = "none";
+}
+
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 var tbl = document.getElementById('table')
@@ -8,8 +16,16 @@ tbl.setAttribute('border', '1');
 container.appendChild(tbl);
 app.appendChild(container);
 
-const dropdown = document.getElementById("dropdown");
+const steps = document.getElementById("steps");
+const options = document.getElementById("options");
 
+steps.addEventListener('change', function(e) {
+  showLeaderboard(e);
+})
+
+options.addEventListener('change', function(e) {
+  showLeaderboard(e);
+})
 
 
 var request1 = new XMLHttpRequest()
@@ -20,17 +36,11 @@ request1.onload = function() {
   if (request1.status >= 200 && request1.status < 400) {
     data1.forEach(step => {
       //console.log(step);
-      const anchor = document.createElement('a');
-      anchor.innerText = step.name;
-      anchor.setAttribute("class", "dropdown-item");
-
-      dropdown.appendChild(anchor);
+      const option = document.createElement('option');
+      //const anchor = document.createElement('a');
+      option.innerText = step.name;
+      steps.appendChild(option);
     })
-
-    $('.dropdown-item').click(function (e) {
-        showLeaderboard(e);
-    });
-
   } else {
     const errorMessage = document.createElement('marquee')
     errorMessage.textContent = `Gah, it's not working!`
@@ -40,14 +50,21 @@ request1.onload = function() {
 request1.send();
 
 function showLeaderboard(e) {
-
+  console.log(e);
+  var step = document.getElementById("steps").value;
+  var option = document.getElementById("options").value;
   var oldTr = document.getElementById("tr");
   if(oldTr !== undefined && oldTr !== null) {
     oldTr.remove();
   }
   //leaderboard step 1
   var request = new XMLHttpRequest()
-  request.open('GET', 'http://pure-brushlands-81405.herokuapp.com/scores/top/' + e.currentTarget.text, true)
+  if(document.getElementById("options").value === "All") {
+    request.open('GET', 'http://pure-brushlands-81405.herokuapp.com/scores/top/' + step, true);
+  } else {
+    request.open('GET', 'http://pure-brushlands-81405.herokuapp.com/scores/topBoth/' + step + "," + localStorage.getItem("user"), true);
+  }
+
   request.onload = function() {
     // Begin accessing JSON data here
     var data = JSON.parse(this.response)
